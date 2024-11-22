64558670e54998ed442f01c964ead0dcf7176053
Arpit Ranasaria <arpitr@stanford.edu>
2024-11-21 22:25:45 -0800

    Update config.js

--- src/lib/discord/config.js
+++ src/lib/discord/config.js
@@ -7,9 +7,9 @@
 const config = {
 	GUILD_ID: process.env.GUILD_ID,
 	DISCORD_TOKEN: process.env.BOT_TOKEN,
-	DISCORD_CLIENT_ID: process.env.VITE_CLIENT_ID,
+	DISCORD_CLIENT_ID: import.meta.env.VITE_CLIENT_ID,
 	DISCORD_CLIENT_SECRET: process.env.CLIENT_SECRET,
-	DISCORD_REDIRECT_URI: process.env.VITE_BASE_URL + "/api/discord-oauth-callback",
+	DISCORD_REDIRECT_URI: import.meta.env.VITE_BASE_URL + "/api/discord-oauth-callback",
 	COOKIE_SECRET: process.env.COOKIE_SECRET,
 };
 
