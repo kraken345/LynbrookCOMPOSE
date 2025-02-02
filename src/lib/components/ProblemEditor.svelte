e275166a983ff8845ec435938d3546b987559f0b
Arpit Ranasaria <arpitr@stanford.edu>
2025-02-02 13:24:19 -0800

    Update ProblemEditor.svelte

--- src/lib/components/ProblemEditor.svelte
+++ src/lib/components/ProblemEditor.svelte
@@ -538,7 +538,7 @@
 					submittedText = isDraft ? "Draft Saved" : "Problem Submitted";
 				}
 			} else {
-				isDisabled = false; //Enable editing
+				isDisabled = false;
 
 				throw new Error("Not all the required fields have been filled out");
 			}
